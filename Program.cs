856012b0c9aad79519f0cf671236306f69d70382
Samane Babaei <s.babaei@sevensenders.com>
2023-11-17 09:33:12 +0100

    CPT-3328-update-sqs-usage-examples-include-removing-messages

--- Program.cs
+++ Program.cs
@@ -39,8 +39,39 @@ namespace sqs_sample_dotnet
                     foreach (var message in messages) {
                         Console.WriteLine("Message ID: " + message.MessageId);
                         Console.WriteLine(message.Body + "\n\n");
-                        
+
+                        // Delete the message
+                        var deleteRequest = new DeleteMessageRequest
+                        {
+                            QueueUrl = "https://analytics-api.7senders.com/queue.xml",
+                            ReceiptHandle = message.ReceiptHandle
+                        };
+
+                        await client.DeleteMessageAsync(deleteRequest);
+                        Console.WriteLine($"Message ID: {message.MessageId} deleted.\n");
                     }
+
+                    // Delete messages in batch
+                    var entries = messages.Select(m => new DeleteMessageBatchRequestEntry
+                    {
+                        Id = m.MessageId,
+                        ReceiptHandle = m.ReceiptHandle
+                    }).ToList();
+
+                    var deleteBatchRequest = new DeleteMessageBatchRequest
+                    {
+                        QueueUrl = "https://analytics-api.7senders.com/queue.xml",
+                        Entries = entries
+                    };
+
+                    var deleteBatchResponse = await client.DeleteMessageBatchAsync(deleteBatchRequest);
+                    foreach (var successEntry in deleteBatchResponse.Successful)
+                    {
+                        Console.WriteLine($"Message ID: {successEntry.Id} deleted in batch.\n");
+                    }
+
+                    // ...
+
                 })
                 .GetAwaiter().GetResult();
         }
